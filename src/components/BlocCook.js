0fdb07c0edc43c8917541a83a3cc76173aa6b1b6
Istinnie <n.istinnie.velonkasina@gmail.com>
2023-03-30 16:31:00 +0400

    First commit

---
+++ src/components/BlocCook.js
@@ -0,0 +1,44 @@
+import React from 'react';
+
+const BlocCook = ({c}) => {
+    
+   console.log(c);
+    return (
+        <div className='bloc-cook'>
+          <div>
+                <div >
+                    <h2>{c.strMeal}</h2>
+                </div> 
+                <div>
+                    <h4>Orgine : {c.strArea}</h4>
+                </div>
+                <div className="img-container">
+                    <img src={c.strMealThumb} alt="" /> 
+                </div>
+                <div className="info-container">
+                    <span>
+                        {c.strInstructions}
+                    </span>
+                </div>
+            </div>    
+        </div>
+        
+        
+        // <div className='bloc-cook'>
+        //     <div >
+        //         <h2>{objectCook[0].strMeal}</h2>
+        //     </div> 
+        //     <div className="img-container">
+        //         <img src={objectCook[0].strMealThumb} alt="" /> 
+        //     </div>
+        //     <div className="info-container">
+        //         <span>
+        //             {objectCook[0].strInstructions}
+        //         </span>
+        //     </div>
+            
+        // </div>
+    );
+};
+
+export default BlocCook;
