11f345c50b5389a9db92da6a4b112aa2b30af2e0
Istinnie <n.istinnie.velonkasina@gmail.com>
2023-08-28 13:22:53 +0400

    update project

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,52 +1,48 @@
-import axios from 'axios';
-import React, { useEffect, useState } from 'react';
-import BlocCook from '../components/BlocCook';
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import BlocCook from "../components/BlocCook";
 
 const HomePage = () => {
-    let [cooks, setCooks]=useState([]);
-    let [searchInput, setSearchInput]=useState("");
-    
-    // https://www.themealdb.com/api/json/v1/1/search.php?s=" + searchInput
-    
-    const fetchCooks=()=>{
-        axios
-        .get("https://www.themealdb.com/api/json/v1/1/search.php?s="+searchInput)
-        .then((response)=>setCooks(response.data.meals));
-    }
-    
-    useEffect(fetchCooks,[]);
+  let [cooks, setCooks] = useState([]);
+  let [searchInput, setSearchInput] = useState("");
 
-    return (
-        <div className="App">
-            <header className="App-header">
-                <h1>Cooking recipes</h1>
-                <input 
-                    type="text" 
-                    id="searchInput" 
-                    onChange={(e)=>setSearchInput(e.target.value)}
-                    placeholder="Tappez le nom de la recette"
-                />
-            </header>
-            <section className="cooks-container">
-                {
-                    cooks
-                    .filter((cook)=>
-                        cook.strMeal
-                        .toLowerCase()
-                        .includes(searchInput.toLowerCase())
-                    ).map((cook,index)=>(
-                        <BlocCook key={index} c={cook}/>
-                    ))
-                    // <BlocCook  c={cooks}/>
-                }
-                    
-                
-                
-                
-                
-            </section>
-        </div>
-    );
+  // https://www.themealdb.com/api/json/v1/1/search.php?s=" + searchInput
+
+  const fetchCooks = () => {
+    axios
+      .get(
+        "https://www.themealdb.com/api/json/v1/1/search.php?s=" + searchInput
+      )
+      .then((response) => setCooks(response.data.meals));
+  };
+
+  useEffect(fetchCooks, []);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Cooking recipes</h1>
+        <input
+          type="text"
+          id="searchInput"
+          onChange={(e) => setSearchInput(e.target.value)}
+          placeholder="Tappez le nom de la recette"
+        />
+      </header>
+      <section className="cooks-container">
+        {
+          cooks
+            .filter((cook) =>
+              cook.strMeal.toLowerCase().includes(searchInput.toLowerCase())
+            )
+            .map((cook, index) => (
+              <BlocCook key={index} c={cook} />
+            ))
+          // <BlocCook  c={cooks}/>
+        }
+      </section>
+    </div>
+  );
 };
 
 export default HomePage;
